a66f912fcf10d421689b776fea730b09a9bcbd7b
jefsteward <jeffrystewardw@gmail.com>
2020-07-14 10:22:01 +0700

    Initial Commit

---
+++ mc3-investor-app/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  mc3-investor-app
+//
+//  Created by Jeffry Steward W on 14/07/20.
+//  Copyright © 2020 Jeffry Steward W. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ mc3-investor-app/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  mc3-investor-app
+//
+//  Created by Jeffry Steward W on 14/07/20.
+//  Copyright © 2020 Jeffry Steward W. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
